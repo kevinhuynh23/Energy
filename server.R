0882e5d871862c36e1d68cf89a2b4bb4ee5cb8c5
Sagar Surana <Sagar@D-173-250-246-246.dhcp4.washington.edu>
2017-12-04 19:07:20 -0800

    total power added to state energy dataset

--- server.R
+++ server.R
@@ -1,25 +1,30 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+library("shiny")
+library("dplyr")
+library("leaflet")
 
-library(shiny)
-
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+  
+  state_data <- read.csv("data/stateEnergyData.csv", stringsAsFactors = FALSE)
+  company_data <- read.csv("data/companies_and_state.csv", stringsAsFactors = FALSE)
+  state_coords <- read.csv("data/States_Coordinates.csv", stringsAsFactors = FALSE)
+  
+  all_state_info <- full_join(state_data, state_coords, by = "State") %>% 
+                    select(State, Photovoltaic, CSP, OnshoreWind, OffshoreWind,
+                           BiopowerSolid, BioPowerGaseous, GeothermalHydrothermal,
+                           EGSGeothermal, Hydropower, Latitude, Longitude)
+  
+  output$GETNAMEFROMUI <- renderLeaflet({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #input$check for energy
+    #input$dropdown for companies
+    #input$dropdown2 for states
+    #input$slider for power
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    chosen_state <- all_state_info %>% filter(State == input$dropdown2)
+    
+    
+    
+    #map code
     
   })
   
