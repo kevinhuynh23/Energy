40fcdc049570bf2ddce192039360ce32e6bd3e3f
Sagar Surana <Sagar@D-173-250-130-12.dhcp4.washington.edu>
2017-12-06 01:07:33 -0800

    all things done. except remove slider from ui

--- server.R
+++ server.R
@@ -7,7 +7,6 @@ library(lattice)
 library(plotly)
 library("ggplot2")
 
-
 shinyServer(function(input, output) {
   
   state_data <- read.csv("data/stateEnergyData.csv", stringsAsFactors = FALSE)
@@ -21,11 +20,6 @@ shinyServer(function(input, output) {
            EGS.Geothermal, Hydropower, Latitude, Longitude)
 
   output$map <- renderPlotly({
-    
-    #input$type for energy
-    #input$company for companies
-    #input$state for states
-    #input$power for power
     
     capFirst <- function(s) {
       gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", s, perl=TRUE)
@@ -44,20 +38,14 @@ shinyServer(function(input, output) {
       #chosen_state_national table will contain info only about that state
    } else {
       #if National is chosen
-     chosen_state_national <- all_state_info
+      chosen_state_national <- all_state_info
       #chosen_state_national table will contain info about all states
    }
     #the name of the data frame created, whether a state is chosen or National, will be the same
-
-    #when national is chosen states are filtered out according to the slider's min and max Total Power levels
-    #if one state is chosen, it will be shown if still in the range otherwise a blank map will be shown
-    state_info_with_slider <- chosen_state_national %>% filter(Total.Power > input$power) %>%
-      filter(Total.Power < input$power)
     
     #once an energy type is selected this data frame will contain data for either
     # all states (if National) or a single state (if state chosen) for only
     #the selected energy type
-    
     selected_energy_state <- chosen_state_national %>% select(State, input$type, Longitude, Latitude)
     
     colnames(selected_energy_state)[2] <- 'EnergyType'
@@ -65,38 +53,26 @@ shinyServer(function(input, output) {
     choropleth2 <- choropleth %>% select(lng, lat, State, input$type, group)
     colnames(choropleth2)[4] <- 'SelectedEnergyType'
     
-    
-    
     #once a company is selected this data frame will contain
     #all the states which the company covers
     #the state names are abbreviated in this data frame
     selected_states_for_company <- company_data %>% 
-      filter(Utility.Name == input$company) 
-    
-    
-    
+                                   filter(Utility.Name == input$company) 
     stateCompany <- all_state_info %>%
-      filter(all_state_info$State == selected_states_for_company$State)
-      
+                    filter(all_state_info$State == selected_states_for_company$State)
     companyInfo <- inner_join(stateCompany, selected_states_for_company, by = "State")
     
     p <- ggplot() 
-    g <- p + geom_polygon(data = choropleth2, aes(lng, lat, group = group, fill = SelectedEnergyType)) +
+    g <- p + geom_polygon(data = choropleth2, aes(lng, lat, group = group, fill = SelectedEnergyType)) + 
       geom_point(aes(x = companyInfo$Longitude, y = companyInfo$Latitude,                     
-                text = companyInfo$Utility.Name), 
-                colour="red", alpha = 1/2, size=3) +
-      geom_point(data = chosen_state_national, aes(x = Longitude, y = Latitude), colour = 'white') +
-      xlab('Longitude') + 
-      ylab('Latitude')
-    
+                                        text = companyInfo$Utility.Name), 
+                                       colour="red", alpha = 1/2, size=3) +
+     geom_point(data = chosen_state_national, aes(x = Longitude, y = Latitude), colour = 'white')
+      #xlab('Longitude') + 
+    #  ylab('Latitude')
    g <- ggplotly(g)
-    
-  
    g <- plotly_build(g)
-   
-    
   })
   output$energyTable <- DT::renderDataTable({all_state_info})  
-
 })
 
